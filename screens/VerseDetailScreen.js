b5770fdf0a2da81b2129ad9e1863ca8419c1fea6
StanimalTheMan <sjc591@nyu.edu>
2023-05-03 16:14:31 -0400

    Try to add bible verse number on top of verse detail screen via useLayoutEffect

--- screens/VerseDetailScreen.js
+++ screens/VerseDetailScreen.js
@@ -1,15 +1,24 @@
 import { View, Text, StyleSheet } from "react-native";
 import { VERSES } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
 
 function VerseDetailScreen({ route, navigation }) {
   const verseId = route.params.verseId;
 
   const selectedVerse = VERSES.find((verse) => verse.id === verseId);
-
   let versesText =
     selectedVerse.verses.length > 1
-      ? `${selectedVerse.verses[0]}-${verses[verses.length - 1]}`
+      ? `${selectedVerse.verses[0]}-${
+          selectedVerse.verses[selectedVerse.verses.length - 1]
+        }`
       : `${selectedVerse.verses[0]}`;
+  const verseNumberFormat = `${selectedVerse.book} ${selectedVerse.chapter}:${versesText} ${selectedVerse.version}`;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: { verseNumberFormat },
+    });
+  }, []);
 
   return (
     <View style={styles.verseContainer}>
@@ -17,10 +26,7 @@ function VerseDetailScreen({ route, navigation }) {
         <Text>{selectedVerse.content}</Text>
       </View>
       <View>
-        <Text style={styles.verse}>
-          - {selectedVerse.book} {selectedVerse.chapter}:{versesText}{" "}
-          {selectedVerse.version}
-        </Text>
+        <Text style={styles.verse}>- {verseNumberFormat}</Text>
       </View>
     </View>
   );
