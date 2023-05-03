b8a8f103c890dd2760951fa18b2549b7ca7dd75a
StanimalTheMan <sjc591@nyu.edu>
2023-05-02 20:05:03 -0400

    set up basic navigation with dummy view for topics screen

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import TopicsScreen from "./screens/TopicsScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Topics" component={TopicsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
---
+++ screens/TopicsScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function TopicsScreen() {
+  return (
+    <View>
+      <Text>Topics Screen</Text>
+    </View>
+  );
+}
+
+export default TopicsScreen;
